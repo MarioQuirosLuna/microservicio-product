781e4503373e32ee83364dbf95a25c18ff73b1a7
MarioQL <mquiros@epromcr.com>
2025-01-03 09:17:14 -0600

    Creacion proyecto con spring y microservicios - java v21 -springboot v3.4.1

---
+++ src/main/java/com/example/springcloud/microservicio/product/MicroservicioProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.microservicio.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcloud/microservicio/product/microservicio_product/MicroservicioProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.microservicio.product.microservicio_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
