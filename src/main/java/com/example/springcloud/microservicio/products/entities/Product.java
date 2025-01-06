cd18c0ca970a1bce5b857f784c01502a67c9d69b
MarioQL <mquiros@epromcr.com>
2025-01-06 12:00:14 -0600

    COnfiguracion de puertos para balanceador de carga

--- src/main/java/com/example/springcloud/microservicio/products/entities/Product.java
+++ src/main/java/com/example/springcloud/microservicio/products/entities/Product.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name="PRODUCTS")
@@ -24,60 +25,47 @@ public class Product {
     @Column(name = "CREATE_AT")
     private LocalDate createAt;
 
-    /**
-     * @return Long return the id
-     */
+    @Transient
+    private int port;
+
     public Long getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
     public void setId(Long id) {
         this.id = id;
     }
 
-    /**
-     * @return String return the name
-     */
     public String getName() {
         return name;
     }
 
-    /**
-     * @param name the name to set
-     */
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * @return Double return the price
-     */
     public Double getPrice() {
         return price;
     }
 
-    /**
-     * @param price the price to set
-     */
     public void setPrice(Double price) {
         this.price = price;
     }
 
-    /**
-     * @return LocalDate return the createAt
-     */
     public LocalDate getCreateAt() {
         return createAt;
     }
 
-    /**
-     * @param createAt the createAt to set
-     */
     public void setCreateAt(LocalDate createAt) {
         this.createAt = createAt;
     }
 
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+    
 }
