75e3ce0254e8edec2daebd465c3ea7877162bc7d
MarioQL <mquiros@epromcr.com>
2025-01-06 16:11:52 -0600

    Configuracion de rutas para usar Gateway Server

--- src/main/java/com/example/springcloud/microservicio/products/controllers/ProductController.java
+++ src/main/java/com/example/springcloud/microservicio/products/controllers/ProductController.java
@@ -11,11 +11,8 @@ import com.example.springcloud.microservicio.products.entities.Product;
 import com.example.springcloud.microservicio.products.services.ProductService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @RestController
-@RequestMapping("/api/products")
 public class ProductController {
     
     @Autowired
