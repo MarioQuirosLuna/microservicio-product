3d78d895db125a0f51940b1c5006b74a4b327ec4
MarioQL <mquiros@epromcr.com>
2025-01-07 11:55:12 -0600

    Circuit breaker customizado para manejo de errores

--- src/main/java/com/example/springcloud/microservicio/products/controllers/ProductController.java
+++ src/main/java/com/example/springcloud/microservicio/products/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.example.springcloud.microservicio.products.controllers;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +26,17 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> detailsProduct(@PathVariable Long id) {
+    public ResponseEntity<Product> detailsProduct(@PathVariable Long id) throws InterruptedException {
+        if(id.equals(10L)){
+            System.out.println("Error simulado con id 10 para pruebas Resilience4J!");
+            throw new IllegalStateException("Error simulado con id 10 para pruebas Resilience4J!");
+        }
+
+        if(id.equals(7L)){
+            System.out.println("Lentitud de respuesta simulada con id 7 para pruebas Resilience4G!");
+            TimeUnit.SECONDS.sleep(5L);
+        }
+        
         Optional<Product> productOptional = service.findById(id);
         if(productOptional.isPresent()){
             return ResponseEntity.ok(productOptional.orElseThrow());
